77c6c5ec59ee71fa9fe546aaecc79244adbb22f6
Adalyat Nazirov <aenazirov@gmail.com>
2021-04-19 22:16:14 +0300

    certification test E1

---
+++ Certification/FilledOrderImmediatelyAlgorithm.cs
@@ -0,0 +1,56 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using QuantConnect.Brokerages;
+using QuantConnect.Data;
+using QuantConnect.Interfaces;
+using QuantConnect.Orders;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using QuantConnect.Algorithm;
+using QuantConnect.Scheduling;
+
+namespace QuantConnect.Atreyu.Certification
+{
+    /// <summary>
+    /// E1 - Filled Order
+    /// An immediately filled order
+    /// </summary>
+    /// <meta name="tag" content="using data" />
+    /// <meta name="tag" content="using quantconnect" />
+    /// <meta name="tag" content="trading and orders" />
+    public class FilledOrderImmediatelyAlgorithm : BasicTemplateAlgorithm
+    {
+        protected override string TestCode { get; } = "E1";
+
+        protected override string[] Tickers
+        {
+            get
+            {
+                return new[] { "GOOG" };
+            }
+        }
+
+        public override void OnOrderSubmitted(OrderEvent orderEvent)
+        {
+            Executions[orderEvent.Symbol.Value].Executions = new Queue<ExecutionEvent>(new[]
+            {
+                new ExecutionEvent {Status = OrderStatus.Filled, FillQuantity = 100}
+            });
+        }
+    }
+}
